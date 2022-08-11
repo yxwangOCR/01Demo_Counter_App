81625446b3a00aa48799dbd1173a8efc38efacf5
Hello Git <wang.yixuan@live.fr>
2022-08-11 15:38:44 -0400

    Counter APP using redux, without Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+//useSelector hook helps us to get counter from store.
+//useDispatch hook helps us to dispatch action,
+//The store fetch action, then update store.
+
 
 function App() {
+  
+  //Provide a callback function with "state" inside
+  //We have coounter property in state
+const counter = useSelector((state)=>state.counter)
+const dispatch = useDispatch()
+const increment =()=>{
+  dispatch({type: "INCREMENT"})
+};
+const decrement =()=>{
+  dispatch({type: "DECREMENT"})
+};
+
+const addByTen = ()=>{
+  dispatch({type: "ADD", payload: 10})
+}
+
+const subtractByTen =()=>{
+  dispatch({type:"SUB", payload: 10})
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <div className='buttons'>
+      <button onClick={addByTen}>+ 10</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>   
+      <button onClick={subtractByTen}>- 10</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// Dispatch an action : whenever we want to send data to store
+// Dispatch an unique action, then store will fetch this action and do update.
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store'// Import "store" for <Provider>'s prop use
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // Wrap all app components with <Provider>
+  // Provider contain a prop : store
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
